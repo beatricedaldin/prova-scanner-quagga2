42592d972e97e8670defda4e539ad1cf49c48ebb
Beatrice Daldin <beadaldin@gmail.com>
2024-06-13 09:48:28 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewChild } from '@angular/core';
+import { BarcodeScannerLivestreamComponent } from 'ngx-barcode-scanner';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+@ViewChild(BarcodeScannerLivestreamComponent)
+  barcodeScanner: BarcodeScannerLivestreamComponent;
+
+  barcodeValue;
+
+  // types = [
+  //   "ean",
+  //   "code_128"
+  // ]
+
+  ngAfterViewInit() {
+    this.barcodeScanner.start();
+  }
+
+  onValueChanges(result) {
+    this.barcodeValue = result.codeResult.code + " - " + result.codeResult.format;
+  }
+
+  onStarted(started) {
+    console.log(started);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarcodeScannerLivestreamModule } from 'ngx-barcode-scanner';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BarcodeScannerLivestreamModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
